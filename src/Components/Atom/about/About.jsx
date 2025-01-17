8c4ff8d902c9d4a91240c4e0d8db2e02b0de304c
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-01-18 02:07:24 +0400

    feat(react file) add responses

--- src/Components/Atom/about/About.jsx
+++ src/Components/Atom/about/About.jsx
@@ -11,6 +11,8 @@ function About() {
     Great
     </h1>
     <p className="about_paragraph">Your performance exceed 65% <br />of the people conducting the <br />test here!</p>
+    <p className="about_paragraph1">Your performance exceed 65% <br />of the people conducting the test here!</p>
+
 </div>
         </>
     )
